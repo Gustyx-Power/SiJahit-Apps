61201cabae06c1b44dfa0be575fa34f974a61e52
Gustyx-Power <gustiadityamuzaky08@gmail.com>
2025-07-03 20:28:18 +0700

    #21 Add Firebase Database and Google Auth dependencies
    This commit introduces the necessary dependencies for Firebase Realtime Database Kotlin extensions and Google Play Services Authentication.
    
    - Added `implementation(libs.google.auth)` and `implementation(libs.firebase.database.ktx)` to `app/build.gradle.kts`.
    - Added version definitions for `play-services-auth` and `firebaseDatabaseKtx` in `gradle/libs.versions.toml`.
    - Updated `app/google-services.json` with Firebase Realtime Database URL and OAuth client configurations for Google Sign-In.
    - Enabled parallel model fetch in `.idea/gradle.xml`.
    - Updated `.idea/misc.xml` by removing `ExternalStorageConfigurationManager`.
    - Set "Firebase Crashlytics" as the selected tab in `.idea/appInsightsSettings.xml`.
    
    Signed-off-by: Gustyx-Power <gustiadityamuzaky08@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.firebase.auth)
+    implementation(libs.google.auth)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
