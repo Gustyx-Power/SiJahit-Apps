631233697d45d8bc3e674d3976ce84c8e88d333c
ChandraRasmitaPutra <chandrarasmita03@gmail.com>
2025-07-07 23:24:53 +0700

    #42 Model: Add Pesanan data class
    This commit introduces the `Pesanan` data class.
    
    The `Pesanan` data class includes the following properties:
    - `id` (String)
    - `nama` (String)
    - `tanggal` (String)
    - `total` (Long)
    - `status` (String)
    
    Signed-off-by: ChandraRasmitaPutra <chandrarasmita03@gmail.com>

---
+++ app/src/main/java/id/kel3/sijahit/sijahit/model/Pesanan.kt
@@ -0,0 +1,9 @@
+package id.kel3.sijahit.sijahit.model
+
+data class Pesanan(
+    val id: String = "",
+    val nama: String = "",
+    val tanggal: String = "",
+    val total: Long = 0,
+    val status: String = ""
+)
